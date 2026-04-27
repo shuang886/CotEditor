a7c61fc53ae8382308562bc5b2508217f376a51a
Steven Huang <140762048+shuang886@users.noreply.github.com>
2026-04-27 16:33:53 +0100

    first stab at tree-sitter-lilypond integration

---
+++ Packages/Syntax/Sources/SyntaxParsers/Queries/LilyPond/highlights.scm
@@ -0,0 +1,89 @@
+(comment) @comments
+
+(punctuation) @attributes
+
+(
+  (assignment_lhs) @variables
+  .
+  (
+    (punctuation) @keywords
+    (#match? @keywords "^=$")
+  )
+)
+
+(named_context
+  (symbol)
+  .
+  (
+    (punctuation) @keywords
+    (#match? @keywords "^=$")
+  )
+  .
+  [(symbol) (string)]
+)
+
+(chord
+  .
+  "<" @keywords
+  ">" @keywords
+  .
+)
+
+(
+  (escaped_word) @types
+  (#not-match? @types "^\\\\(?:include|maininput|version)$") ; This is needed for Panic Nova
+)
+(
+  (escaped_word) @commands
+  (#match? @commands "^\\\\(?:include|maininput|version)$") ; These are handled directly by LilyPond’s lexer.
+)
+(
+  (escaped_word) @values
+  (#match? @values "^\\\\(?:breve|longa|maxima)$")
+)
+(
+  (escaped_word) @characters
+  (#match? @characters "^\\\\\\^$")
+)
+
+(quoted_identifier
+  "\"" @keywords
+)
+
+(
+  (symbol) @keywords
+  (#match? @keywords "^q$")
+)
+
+[
+  (fraction)
+  (decimal_number)
+  (unsigned_integer)
+] @numbers
+
+(dynamic) @characters
+
+(instrument_string_number) @characters
+
+(
+  (string
+    "\"" @string.delimiter.left
+    [
+      (string_fragment)?
+      (escape_sequence)? @string.escape
+    ]
+    "\"" @string.delimiter.right
+  )
+) @strings
+
+[
+  "{" "}"
+  "<<" (parallel_music_separator) ">>"
+  "#{" "#}"
+] @keywords
+
+(chord
+  ">>" @comments
+)
+
+(embedded_scheme_prefix) @commands
